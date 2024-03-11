ecad801dcce554b70ec1dd4c10143b4a779f14fb
siva3150 <siva123@gmail.com>
2024-03-11 19:44:04 +0530

    destroy

---
+++ terraform/dev/backend.tf
@@ -0,0 +1,4 @@
+bucket = "sivadevops-state-dev"
+key    = "user"
+region = "us-east-1"
+dynamodb_table = "sivadevops-locking-dev"
---
+++ terraform/prod/backend.tf
@@ -0,0 +1,4 @@
+bucket = "sivadevops-state-prod"
+key    = "user"
+region = "us-east-1"
+dynamodb_table = "sivadevops-locking-prod"
